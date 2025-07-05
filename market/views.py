4337ca3a8d36def596d4acb5524a13596e1da416
Pinaki044 <pinakipriya2k@gmail.com>
2025-07-06 03:52:55 +0530

    Build homepage, templates, static CSS, models, and DB setup

--- market/views.py
+++ market/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import Type, Item, SHG
 
 def home(request):
-    return render(request, 'market/home.html')
+    categories = Type.objects.all()
+    new_items = Item.objects.order_by('-id')[:5]
+    shg_stories = SHG.objects.all()[:3]  # can limit or filter as needed
+
+    context = {
+        'categories': categories,
+        'new_items': new_items,
+        'shg_stories': shg_stories,
+    }
+    return render(request, 'market/home.html', context)
+
